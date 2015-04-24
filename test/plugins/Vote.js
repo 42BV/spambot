67ae3019bdbdd7a6b51965a08331a9310b23a09b
PNikray <poujanik@gmail.com>
2015-04-24 10:34:53 +0200

    Added sinon to the dev dependencies.
    Sinon can be used to spy on methods.

--- test/plugins/Vote.js
+++ test/plugins/Vote.js
@@ -1,24 +1,24 @@
 var Vote = require('../../src/plugins/Vote.js');
-var should = require('should'); // jshint ignore:line
-
+var sinon = require('sinon');
 var vote;
+var bot = {
+    onMessage: function() {},
+    message: function() {}
+};
 describe('Vote', function() {
     beforeEach(function() {
-        vote = new Vote({
-            onMessage: function() {},
-            message: function() {}
-        });
+        vote = new Vote(bot);
     });
-    describe('onMessage', function() {
-        it('should call the correct function', function(done){
-            vote.startPoll = function(){
+    describe('#onMessage', function() {
+        it('should call the correct function', function(done) {
+            vote.startPoll = function() {
                 done();
             };
 
             vote.onMessage(null, null, '!poll start ');
         });
     });
-    describe('start poll', function() {
+    describe('#start poll', function() {
         it('should create a new poll', function() {
             vote.startPoll(null, null, '!poll start theAwesomePollName');
             vote.polls.should.have.property('theAwesomePollName');
@@ -30,7 +30,7 @@ describe('Vote', function() {
             vote.polls.should.have.lengthOf(0);
         });
     });
-    describe('should register a vote properly', function() {
+    describe('#startPoll', function() {
         it('should register yes', function() {
             vote.startPoll(null, null, '!poll start theAwesomePollName');
             vote.vote(null, null, '!vote theAwesomePollName yes');
@@ -48,11 +48,24 @@ describe('Vote', function() {
             vote.polls.theAwesomePollName.yes.should.be.equal(0);
         });
     });
-    describe('end poll', function() {
+    describe('#endPoll', function() {
         it('should properly stop a poll', function() {
             vote.startPoll(null, null, '!poll start theAwesomePollName');
             vote.endPoll(null, null, '!poll end theAwesomePollName');
             vote.polls.should.have.lengthOf(0);
+        });
+    });
+    describe('#poll', function() {
+        it('Should give an error when the poll does not exists', function() {
+            bot.message = sinon.spy();
+            vote.poll('channel', null, '!poll test');
+            bot.message.calledWith('channel', '404 test not found.').should.be.ok;
+        });
+        it('Should return the poll info', function() {
+            vote.startPoll(null, null, '!poll start awesome');
+            bot.message = sinon.spy();
+            vote.poll('channel', null, '!poll awesome');
+            bot.message.calledWith('channel', sinon.match(/The current score for/)).should.be.ok;
         });
     });
 });
