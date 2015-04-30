046dc9c15670b919d8ed6f436f24043c0d15091c
PNikray <poujanik@gmail.com>
2015-04-30 17:15:49 +0200

    Added config package. Under /config the credentials will be stored

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 var Wobot = require('wobot').Bot;
-var minimist = require('minimist')(process.argv.slice(2));
 var Hipchatter = require('hipchatter');
 var Bot = require('./bot.js');
-
-if (!minimist.u || !minimist.p || !minimist.k) {
-    throw 'You should give the jid (-u), password (-p) and the api key (-k) for the bot account or your own account.';
-}
+var config = require('config');
 
 // Initialize wobot, hipchatter and the bot
 var wobot = new Wobot({
-    jid: minimist.u,
-    password: minimist.p
+    jid: config.jid,
+    password: config.password
 });
-var hipchatter = new Hipchatter(minimist.k);
+var hipchatter = new Hipchatter(config.apiKey);
 var bot = new Bot(wobot, hipchatter);
 
 var intervalId;
@@ -21,9 +17,6 @@ wobot.connect();
 wobot.onConnect(function() {
     // Load the plugins
     require('./manager.js')(bot);
-
-    // Join the test room for debuggin purposes
-    this.join('69596_api_test_room@conf.hipchat.com');
 
     // Fetch the rooms at regular interval
     intervalId = setInterval(bot.setRooms, 2000);
