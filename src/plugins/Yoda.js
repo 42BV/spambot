6c73b1360db787068cef0404d3c4ef4adb47cac2
PNikray <poujanik@gmail.com>
2015-06-09 16:52:30 +0200

    Fixed the yoda plugin. For some reason the _startsWith does not work. Replaced it with regex

--- src/plugins/Yoda.js
+++ src/plugins/Yoda.js
@@ -1,35 +1,36 @@
-/**
- * A simple wobot plugin that will display a sentence in yoda.
- * Talk in yoda by: !yoda <<message>>
- */
 
 var request = require('request');
-var _ = require('lodash');
-var debugErr = require('debug')('plugin:yoda:error');
 
 module.exports = function(bot) {
 
-  bot.onMessage(function(channel, from, message) {
-    if (_.startsWith('!yoda', message)) {
-      var sentence = message.substring(5, message.length).trim().split(' ').join('+');
-      var options = {
-        url: 'https://yoda.p.mashape.com/yoda?sentence=' + sentence,
-        headers: {
-          'X-Mashape-Key': 'YbkavLFoacmshyxuRzywaXEucUfyp1CTGEsjsnGAtcVzJ8TCJq'
+    /**
+    * A simple wobot plugin that will display a sentence in yoda.
+    * Talk in yoda by: !yoda <<message>>
+    */
+    bot.onMessage(function(channel, from, message) {
+        if (/^!yoda (.*)/i.test(message)) {
+            var sentence = message.substring(5, message.length).trim().split(' ').join('+');
+            var options = {
+                url: 'https://yoda.p.mashape.com/yoda?sentence=' + sentence,
+                headers: {
+                    'X-Mashape-Key': 'YbkavLFoacmshyxuRzywaXEucUfyp1CTGEsjsnGAtcVzJ8TCJq'
+                }
+            };
+            request(options, function(error, response, body) {
+                if (!error) {
+                    bot.send({
+                        jid: channel,
+                        message: '@' + from.split(' ').join('') + ' ' + body
+                    });
+                } else {
+                    console.error('Yoda: Request failed with error %j.', error);
+                    bot.send({
+                        jid: channel,
+                        message: 'Something went wrong when using the yoda plugin.'
+                    });
+                }
+            });
         }
-      };
+    });
 
-      request(options, function (error, response, body) {
-        if (!error) {
-          bot.send({
-              jid: channel,
-              message: '@' + from.split(' ').join('') + ' ' + body
-          });
-        } else {
-          debugErr('Request failed with error %j.', error);
-        }
-      });
-    }
-  });
-  
 };
