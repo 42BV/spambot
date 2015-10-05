8d4c6900dce46d22c09dcc478a3befd8d3dcb163
Pouja Nikray <poujanik@gmail.com>
2015-10-05 15:14:15 +0200

    Added missing comma. I really need to stop doing these hotfixes and to a proper fix...

--- src/plugins/Help.js
+++ src/plugins/Help.js
@@ -7,7 +7,7 @@ module.exports = function(bot) {
     var helpMessage = [
         'Hi dev, I\'m here to help you in your daily tasks by providing helpful functions.',
         'Are you missing a feature, please write one and submit a pull request to http://www.github.com/42bv/spambot.',
-        'See https://github.com/42BV/spambot/tree/master/src/plugins on how to write one.'
+        'See https://github.com/42BV/spambot/tree/master/src/plugins on how to write one.',
         'For an overview of all the possible commands see https://github.com/42BV/spambot/blob/master/README.md#commands'
     ].join('\n');
 
