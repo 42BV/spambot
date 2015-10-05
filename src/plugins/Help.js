5117a895234b1befcea27d6c4dde83d6f3f4fac4
Pouja Nikray <poujanik@gmail.com>
2015-10-05 15:11:20 +0200

    Fixed spelling in the help plugin

--- src/plugins/Help.js
+++ src/plugins/Help.js
@@ -6,7 +6,8 @@ module.exports = function(bot) {
 
     var helpMessage = [
         'Hi dev, I\'m here to help you in your daily tasks by providing helpful functions.',
-        'Are you missing a feature, please write on and submit a pull request to http://www.github.com/42bv/spambot.',
+        'Are you missing a feature, please write one and submit a pull request to http://www.github.com/42bv/spambot.',
+        'See https://github.com/42BV/spambot/tree/master/src/plugins on how to write one.'
         'For an overview of all the possible commands see https://github.com/42BV/spambot/blob/master/README.md#commands'
     ].join('\n');
 
